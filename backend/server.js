f0c2b7a413f80621d3f8cce8f282bc6c1279331b
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2026-06-22 23:35:18 +0200

    Set up basic routing to supabase
    - Connection to Supabase working
    - Added basic test for select, insert and update to the sql tables
    - Added cors
    - Created frontend -> backend test route

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,56 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import sql from "./db.js";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/sql", async (req, res) => {
+  const row_values = await display_values();
+  res.send(row_values);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-console.log(`${process.env.TEST}`);
+async function dbInstert() {
+  const test_insert = {
+    name: "Innsmouth Consparicy",
+    start_date: "2025-11-30",
+    status: "Finished",
+  };
 
-async function connection_test() {
-  test = await sql`SELECT NOW()`;
-  return test;
+  await sql`
+insert into test_campaign ${sql(test_insert, "name", "start_date", "status")}`;
 }
-console.log(connection_test);
+
+async function display_values() {
+  const select_campaign = ["name", "start_date", "status"];
+
+  const return_test =
+    await sql`select ${sql(select_campaign)} from test_campaign`;
+
+  return return_test;
+}
+
+display_values();
+
+const update_value = {
+  status: "Test",
+};
+
+async function update_values() {
+  const update_test =
+    await sql`update test_campaign set ${sql(update_value, "status")} where id=2`;
+  display_values();
+}
+
+update_values();
--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,16 @@
-import "dotenv/config";
+async function test_request() {
+  const url = "http://localhost:3000/sql";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Reponse status: ${response.status}`);
+    }
 
-console.log(`${process.env.TEST}`);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+test_request();
